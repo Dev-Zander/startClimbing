dc161ff66d89c1de59e5122d4c7fe9de19813613
Dev-Zander <nx2reign@gmail.com>
2018-06-25 09:36:23 -0700

    Fix for Destroy Redirect

--- server/controllers/loginController.js
+++ server/controllers/loginController.js
@@ -3,6 +3,6 @@ module.exports = {
     destroy:(req,res,next)=>{
         const db = req.app.get('db')
         req.session.destroy()
-        res.redirect('https://alxdev.auth0.com/v2/logout?returnTo=http://localhost:3000/#/')
+        res.redirect(`https://alxdev.auth0.com/v2/logout?returnTo=${process.env.HOMEPAGE}`)
     },
 }
